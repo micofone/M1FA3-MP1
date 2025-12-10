5fbc04dff3d1246532623ba513124e2a6f312614
kyle <133757942+micofone@users.noreply.github.com>
2025-12-10 14:29:06 +0800

    Create mp1script.js

---
+++ mp1script.js
@@ -0,0 +1,10 @@
+function replaceAll(){
+    let str1 = document.getElementById("inputBox").value;
+    console.log(str1);
+
+    let newStr = str1.replaceAll(' ', ' ');
+    document.getElementById("result").innerHTML = newStr;
+}
+
+
+
