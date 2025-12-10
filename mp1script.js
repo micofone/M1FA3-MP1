bf16772816d8b83b94bd3aa72a2cd6e0f7d69751
kyle <133757942+micofone@users.noreply.github.com>
2025-12-10 15:11:16 +0800

    Update mp1script.js

--- mp1script.js
+++ mp1script.js
@@ -2,7 +2,7 @@ function replaceAll(){
     let str1 = document.getElementById("inputBox").value;
     console.log(str1);
 
-    let newStr = str1.replaceAll(' ', ' ');
+    let newStr = str1.replaceAll(' ', '');
     document.getElementById("result").innerHTML = newStr;
 }
 
